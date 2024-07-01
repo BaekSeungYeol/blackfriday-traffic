4d8e833f09f5148657b4b5983a2923840b891d24
baekseungyeol <beck33333@naver.com>
2024-07-02 00:14:28 +0900

    docs(README.md): 요구사항 추가

---
+++ src/main/kotlin/me/whitewin/blackfridaytraffic/BlackfridayTrafficApplication.kt
@@ -0,0 +1,11 @@
+package me.whitewin.blackfridaytraffic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlackfridayTrafficApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlackfridayTrafficApplication>(*args)
+}
---
+++ src/test/kotlin/me/whitewin/blackfridaytraffic/BlackfridayTrafficApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.whitewin.blackfridaytraffic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlackfridayTrafficApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
